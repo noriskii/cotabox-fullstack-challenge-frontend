842697e7928f0e78eba4d7e89b0c145fec6d9f5a
Lucas Augusto <luckaugusto@gmail.com>
2020-07-24 02:11:40 -0300

    API config changed

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333'
+  // IF USING LOCALHOST USE THIS LINE
+  // baseURL: 'http://localhost:3333'
+  // IF PRODUCTION, USE THIS LINE
+  baseURL: 'https://cotabox-fullstack-challenge.herokuapp.com',
 });
 
 export default api;
